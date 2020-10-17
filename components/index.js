fe99c0dc3fc842cc6e5d9ed308def92136d73228
Manthan Patel <manthanpl93@live.com>
2020-10-17 12:56:53 +0530

    New components structure
    Now multiple components can be added from the components path

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import DetailButtons from './DetailButtons'
+import FilterMenu from './FilterMenu'
+import Footer from './Footer'
+import InputBox from './InputBox'
+import OverviewButtons from './OverviewButtons'
+import OverviewTable from './OverviewTable'
+import Pagination from './Pagination'
+import SearchBox from './SearchBox'
+import TitleFrame from './TitleFrame'
+export { DetailButtons, FilterMenu, Footer, InputBox, OverviewButtons, OverviewTable, Pagination, SearchBox, TitleFrame }
